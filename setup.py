97a26b3a8cf14c645e426b311156d9d5d30b5400
admin <admin@git.com>
2018-09-28 09:37:08 +0800

    update

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version     = '0.0.1',
     description = 'intelligent agent',
     author      = 'jack',
+    author_email= '10067748@zte.com.cn',
     packages    = ('pyant', 'pyant.app', 'pyant.builtin', 'pyant.app.bn', 'pyant.app.umebn')
 )
